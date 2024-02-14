2819a4230fe60ac793c475f9f16022703cbf5e34
Alfredo Paez <alfredo123.apne@gmail.com>
2024-02-14 16:31:53 -0500

    CV renewed 2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,29 +17,32 @@ export class AppComponent {
     first: {
       companyName: 'GudarDevs',
       roleTitle: 'Junior Mobile Developer',
-      roleDescription: 'Eyenet Upgrader Project',
+      roleDescription: 'EyeNet Upgrader: A mobile app automates router upgrades via SSH, showcasing expertise in network integration and streamlined automation.',
       roleAchievements: [
-        'I developed a mobile application that connected to a router via SSH to check its version. If the routers version was outdated, the app downloaded the latest version and automatically performed the upgrade process. This experience allowed me to develop skills in integrating network technologies, creating highly functional mobile applications, and implementing custom solutions to improve efficiency and process automation. I also learned how to work with network protocols and how to integrate them into mobile applications.',
+        'Application Development: Develop robust and user-friendly mobile applications for iOS and Android platforms.',
+        'SSH Integration: Utilize SSH to automate router upgrades, streamlining the process and improving efficiency.',
+        'database Management: Manage and maintain databases to ensure data integrity and security.',
+        'Continuous Integration/Continuous Deployment (CI/CD): Implement CI/CD pipelines to automate the deployment process and improve efficiency.',
       ],
       jobDuration: 'May 2021 - Feb 2022',
       roleSkills: [
-        'Python',
-        'Django',
-        'Angular',
+        'Dart',
+        'Flutter',
         'SQL',
         'BASH Scripting',
         'SSH',
-        'Dart',
-        'Flutter',
       ],
     },
     second: {
       companyName: 'Green DNA',
       roleTitle: 'Mid Mobile Developer',
       roleDescription:
-        'Green DNA Mobile App for managing crops processes in general',
+        'Green DNA: Co-developed a Flutter app automating agriculture tasks, providing real-time crop data and IoT alerts.',
       roleAchievements: [
-        'As part of a team, I developed a custom Flutter application aimed at automating agricultural processes. The app rendered all data generated to a web back office, allowing farmers to monitor crop information in real-time. Additionally, the app could connect to IoT devices to receive alerts and notify farmers of any issues. This experience allowed me to collaborate effectively with a team of developers, designers, and other stakeholders, while also developing skills in creating highly functional mobile applications, integrating APIs and IoT devices, and implementing customized automation solutions to improve efficiency and productivity',
+        "API Integration: Integrate APIs to enhance the app's capabilities and facilitate seamless data exchange.",
+        'Real-time Data Rendering: Implement features to render real-time crop data to a user-friendly web interface for farmers.',
+        'Problem-solving: Address challenges related to IoT integration, data rendering, and other aspects of app development through effective problem-solving.',
+        'Quality Assurance: Contribute to testing processes to ensure the app meets high-quality standards and performs reliably.'
       ],
       jobDuration: 'Feb 2022 - Jan 2023',
       roleSkills: ['Dart', 'Flutter', 'Firebase'],
@@ -47,9 +50,13 @@ export class AppComponent {
     third: {
       companyName: 'GudarDevs',
       roleTitle: 'Semi-senior Mobile Developer',
-      roleDescription: 'Eyeride FMS Project',
+      roleDescription: 'Team contributor to a mobile app displaying real-time fleet locations. Implemented role-based access, GetX state management, push notifications, and custom features. Developed collaboration skills, integrating APIs, and optimizing efficiency.',
       roleAchievements: [
-        'As part of a team, I contributed to the development of a mobile application that utilized maps to display real-time locations of fleets of vehicles. The app included role-based access control, implemented GetX as a state manager, and featured push notifications, custom widgets, real-time driver tracking, and a variety of animations. This experience allowed me to develop skills in collaborating with others to create highly functional mobile applications with complex features, integrating APIs, and implementing custom solutions to improve efficiency.',
+        'Real-time Fleet Location Display: Implement features to display real-time locations of fleets on maps within the app.',
+        'Role-based Access: Implement role-based access to ensure secure and efficient data access for different user groups.',
+        'Push Notifications: Implement push notifications to keep users informed and engaged with the app.',
+        "Custom Widgets and Animations: Create and integrate custom widgets and animations to enhance the app's visual appeal and user experience.",
+        'Project Leadership: Take leadership in specific aspects of the project, ensuring timely delivery of assigned tasks and meeting project milestones.'
       ],
       jobDuration: 'Jan 2023 - Currently',
       roleSkills: ['Dart', 'Flutter', 'Android', 'iOS'],
@@ -59,22 +66,22 @@ export class AppComponent {
   skills = {
     first: {
       techName: 'Dart',
-      widthBar: 80,
+      widthBar: 90,
     },
     second: {
       techName: 'Flutter',
       widthBar: 90,
     },
     third: {
-      techName: 'Python',
-      widthBar: 50,
+      techName: 'Firebase',
+      widthBar: 80,
     },
     fourth: {
       techName: 'SQL',
       widthBar: 50,
     },
     fifth: {
-      techName: 'Android',
+      techName: 'TypeScript/JavaScript',
       widthBar: 50,
     },
     sixth: {
@@ -82,7 +89,7 @@ export class AppComponent {
       widthBar: 50,
     },
     seventh: {
-      techName: 'Angular',
+      techName: 'React',
       widthBar: 50,
     },
     eight: {
@@ -90,8 +97,12 @@ export class AppComponent {
       widthBar: 50,
     },
     nine: {
-      techName: 'Firebase',
-      widthBar: 80,
+      techName: 'Android',
+      widthBar: 60,
+    },
+    tenth: {
+      techName: 'Node',
+      widthBar: 70,
     },
   };
   education = {
@@ -107,15 +118,27 @@ export class AppComponent {
       awardDescription:
         "I participated in Hacktoberfest 2022 and made significant contributions to open source projects, which resulted in me being recognized and rewarded for my efforts.'",
     },
+    second: {
+      awardTitle: 'Caribe Game JAM 2023',
+      awardDescription:
+        'I participated in the Caribe Game JAM 2023 and developed a game using flutter flame, the game was recognized for its creativity and innovation.',
+    },
+    third: {
+      awardTitle: 'Flutter Community',
+      awardDescription:
+        'Co-founder of the FlutterBaq, a community of developers who share knowledge and resources to help each other grow and succeed in the field of mobile app development.',
+    },
+    
+    
   };
   languaje = {
     first: {
       languaje: 'Spanish',
-      level: '(Native)',
+      level: ' (Native)',
     },
     second: {
       languaje: 'English',
-      level: '(Professional)',
+      level: ' (Professional)',
     },
   };
   jobList: Array<any> = Object.entries(this.jobs).reverse();
