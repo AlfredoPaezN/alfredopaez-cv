f45f0f271a166185a99568d944beaec55cd1cf25
Alfredo Paez <alfredo123.apne@gmail.com>
2025-04-25 17:39:52 -0500

    Globant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,55 +10,59 @@ declare var progressively: any;
 export class AppComponent {
   ngOnInit() {
     progressively.init({
-      onLoadComplete: function () {},
+      onLoadComplete: function () { },
     });
   }
   jobs = {
     first: {
       companyName: 'GudarDevs',
       roleTitle: 'Junior Mobile Developer',
-      roleDescription: 'EyeNet Upgrader: A mobile app automates router upgrades via SSH, showcasing expertise in network integration and streamlined automation.',
+      roleDescription: 'Developed a mobile app to automate router upgrades via SSH, enhancing network automation.',
       roleAchievements: [
-        'Application Development: Develop robust and user-friendly mobile applications for iOS and Android platforms.',
-        'SSH Integration: Utilize SSH to automate router upgrades, streamlining the process and improving efficiency.',
-        'database Management: Manage and maintain databases to ensure data integrity and security.',
-        'Continuous Integration/Continuous Deployment (CI/CD): Implement CI/CD pipelines to automate the deployment process and improve efficiency.',
+        'Built robust iOS/Android apps.',
+        'Integrated SSH for seamless router upgrades.',
+        'Managed databases to ensure data integrity.',
+        'Implemented CI/CD pipelines for streamlined deployment.',
       ],
       jobDuration: 'May 2021 - Feb 2022',
-      roleSkills: [
-        'Dart',
-        'Flutter',
-        'SQL',
-        'BASH Scripting',
-        'SSH',
-      ],
+      roleSkills: ['Dart', 'Flutter', 'SQL', 'BASH Scripting', 'SSH'],
     },
     second: {
       companyName: 'Green DNA',
       roleTitle: 'Mid Mobile Developer',
-      roleDescription:
-        'Green DNA: Co-developed a Flutter app automating agriculture tasks, providing real-time crop data and IoT alerts.',
+      roleDescription: 'Co-developed a Flutter app automating agriculture tasks with IoT alerts and real-time crop data.',
       roleAchievements: [
-        "API Integration: Integrate APIs to enhance the app's capabilities and facilitate seamless data exchange.",
-        'Real-time Data Rendering: Implement features to render real-time crop data to a user-friendly web interface for farmers.',
-        'Problem-solving: Address challenges related to IoT integration, data rendering, and other aspects of app development through effective problem-solving.',
-        'Quality Assurance: Contribute to testing processes to ensure the app meets high-quality standards and performs reliably.'
+        'Integrated APIs to enhance app functionality.',
+        'Developed real-time crop data rendering for farmers.',
+        'Solved IoT integration and data rendering issues.',
+        'Contributed to quality assurance and app testing.',
       ],
       jobDuration: 'Feb 2022 - Jan 2023',
       roleSkills: ['Dart', 'Flutter', 'Firebase'],
     },
     third: {
       companyName: 'GudarDevs',
-      roleTitle: 'Semi-senior Mobile Developer',
-      roleDescription: 'Team contributor to a mobile app displaying real-time fleet locations. Implemented role-based access, GetX state management, push notifications, and custom features. Developed collaboration skills, integrating APIs, and optimizing efficiency.',
+      roleTitle: 'Advanced Flutter Mobile Developer',
+      roleDescription: 'Contributed to a mobile app displaying real-time fleet locations with role-based access and custom features.',
       roleAchievements: [
-        'Real-time Fleet Location Display: Implement features to display real-time locations of fleets on maps within the app.',
-        'Role-based Access: Implement role-based access to ensure secure and efficient data access for different user groups.',
-        'Push Notifications: Implement push notifications to keep users informed and engaged with the app.',
-        "Custom Widgets and Animations: Create and integrate custom widgets and animations to enhance the app's visual appeal and user experience.",
-        'Project Leadership: Take leadership in specific aspects of the project, ensuring timely delivery of assigned tasks and meeting project milestones.'
+        'Developed real-time fleet location features.',
+        'Implemented role-based access for secure data management.',
+        'Integrated push notifications for user engagement.',
+        'Created custom widgets/animations for improved UX.',
+        'Led specific project aspects, ensuring timely delivery.',
       ],
-      jobDuration: 'Jan 2023 - Currently',
+      jobDuration: 'Jan 2023 - Oct 2024',
+      roleSkills: ['Dart', 'Flutter', 'Android', 'iOS'],
+    },
+    fourth: {
+      companyName: 'Globant',
+      roleTitle: 'Semi-senior Flutter Developer',
+      roleDescription: 'Currently contributing to the development of a high-performance cross-platform mobile app using Flutter.',
+      roleAchievements: [
+        'Contributing to app development with a focus on performance and scalability.',
+        'Working on new features and improving user experience.',
+      ],
+      jobDuration: 'Oct 2024 - Present',
       roleSkills: ['Dart', 'Flutter', 'Android', 'iOS'],
     },
   };
@@ -128,8 +132,8 @@ export class AppComponent {
       awardDescription:
         'Co-founder of the FlutterBaq, a community of developers who share knowledge and resources to help each other grow and succeed in the field of mobile app development.',
     },
-    
-    
+
+
   };
   languaje = {
     first: {
